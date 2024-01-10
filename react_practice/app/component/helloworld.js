5f054a46a2caeff1cbcd1dfa79804a23a95fa54e
Poonam Gahatraj <poonamgtrz@gmail.com>
2024-01-10 11:38:12 +0545

    react_practice

---
+++ react_practice/app/component/helloworld.js
@@ -0,0 +1,22 @@
+import React from 'react';
+function Hellowrld(){
+    return(
+        <>
+         <button>Hello I'm a button</button>
+        <p> jkkkkkkkkkkkk</p>
+        </>
+       
+    )
+}
+
+export default function Hlowrld(){
+    return(
+        <>
+        <h1>this is my first component</h1>
+        <p>xjznkxknknk snxksk</p>
+        <Hellowrld/>
+        </>
+        
+    )
+}
+
---
+++ react_practice/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import  Hlowrld from './component/helloworld'
+
+export default function Home() {
+  return (
+   <div>
+   <Hlowrld/>
+   </div>
+  )
+}
